c741d0dafc8acf963471b105af6354fb6c6c8115
sobhanvafaei <strawb2025@gmail.com>
2015-12-25 14:37:39 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+
+corpora_path = 'D:/corpora/sample.xml'
+#corpora_root = 'corpora/old_hamshahri/separated'
+#CORPORA_ROOT = 'corpora/old_hamshahri/separated'
+corpora_root = 'D:/corpora/hamshahri'
+
+documents_count = 2000
+TOT_DOCS = 4000
+
+classifier_type = 'NaiveBayes'
+CLASSIFIER = 'LinearSVC'
+SELF_TRAINING = False
+
+semi_supervised = True
+unlabeled_documents_count = 1000
